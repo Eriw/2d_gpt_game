31365c445ddbba9a0747af26850ccf29a69656e6
Marcus Wirebrand <wirebrand@gmail.com>
2023-03-25 09:16:47 +0100

    First framework for setting up the basics.

---
+++ main.js
@@ -0,0 +1,29 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game-container',
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('ground', 'assets/ground.png');
+    this.load.image('objects', 'assets/objects_long.png', 'assets/objects_long.json');
+}
+
+function create() {
+    this.add.image(400, 300, 'ground');
+    const mySprite = this.add.sprite(x, y, 'objects', 'sprite1');
+
+    // Set up collisions, animations, and text for quests
+}
+
+function update() {
+    // Update game state, handle user input, and check for quest completion here
+}
